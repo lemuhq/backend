167448623f76a10059e1344dfac67fa435dec40d
Uhembenelson <uhembenelson@gmail.com>
2024-04-15 14:46:24 +0100

    cors

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ import cors from 'cors';
 const port = 5000 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
 app.set('view engine', 'ejs'); //view engine others are hbs, html or react 
 
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
