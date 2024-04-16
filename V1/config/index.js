647a71ecd7aabc8a6de7e6d03ca2d85a39efedeb
Uhembenelson <uhembenelson@gmail.com>
2024-04-16 09:52:34 +0100

    new

--- V1/config/index.js
+++ V1/config/index.js
@@ -1,6 +1,6 @@
-export const  MONGO_URL = "mongodb://localhost/lemu";
+//export const  MONGO_URL = "mongodb://localhost/lemu";
 // lemuhq
 // fHrsdwY8dSAl2yz8
 // rekt krld zuku twbp
-//export const MONGO_URL = "mongodb+srv://lemuhq:fHrsdwY8dSAl2yz8@lemu.4qno9bd.mongodb.net/"
+export const MONGO_URL = "mongodb+srv://lemuhq:fHrsdwY8dSAl2yz8@lemu.4qno9bd.mongodb.net/"
 //export const MONGO_URL = "mongodb+srv://valuefinders101:Mayorgnn088@cluster0.nanvix6.mongodb.net/?retryWrites=true&w=majority"
--- V1/controllers/user/list.js
+++ V1/controllers/user/list.js
@@ -11,11 +11,12 @@ export const saveWaitlist = async (req, res) => {
     let user = await  Waitlist(dataTosave)
     user.save()
     
-     return res.status(200).send({
-         msg: "Login Successful",
-         //accessToken,
-         //data:user
-     })
+    const data = {
+                    message: "Thank you for joining our waitlist! We are on the edge of something new and can't wait to share our updates with you.",
+                    status: true,
+                    data: null
+                }
+                res.status(200).send(data);
   } catch (error) {
     res.status(500).json({ message: 'Error saving waitlist', error });
     console.error('Error saving waitlist', error);
