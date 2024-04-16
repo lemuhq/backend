6eb08989f9850c9eb3c86bf3bd367039779dbda3
Uhembenelson <uhembenelson@gmail.com>
2024-04-16 09:13:01 +0100

    all

--- V1/controllers/user/list.js
+++ V1/controllers/user/list.js
@@ -1,9 +1,9 @@
 import Waitlist from "../../models/Waitlist.js";
 
 export const saveWaitlist = async (req, res) => {
-    
-  try {
     const saveList = new Waitlist(req.body);
+  try {
+    
     await saveList.save();
     const data = {
         message: "Thank you for joining our waitlist! We are on the edge of something new and can't wait to share our updates with you.",
